4913cbeada6d54fd26b23f49e3d59b98cbd04f1d
Trippixn <90400991+trippixn963@users.noreply.github.com>
2026-08-08 10:15:54 -0400

    Release v2.2.0

--- dist/psnppp.user.js
+++ dist/psnppp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PSNP++
 // @namespace    psnppp.trippixn
-// @version      2.1.0
+// @version      2.2.0
 // @description  PSNP+ with local patches, plus two-way cross-device sync for your game lists
 // @author       Trippixn
 // @match        https://psnprofiles.com/*
